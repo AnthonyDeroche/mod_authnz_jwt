a7ef708e53c52605e8078a55bb4d418bce92e162
Anthony Deroche <anthony@deroche.me>
2017-06-24 11:15:13 +0200

    Support null characeters in keys + add tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,12 +6,9 @@ from test_jwt import TestJWT
 
 class TestLogin(TestJWT):
 
-
-    HMAC_SHARED_SECRET_LOGIN = "c2VjcmV0dG9sb2dpbg=="
-
-
-    def test_login_should_success(self):
-        code, content, headers = self.http_post(self.LOGIN_PATH, {self.USERNAME_FIELD:self.USERNAME, self.PASSWORD_FIELD:self.PASSWORD})
+    @TestJWT.with_all_algorithms(algorithms=("HS256", "RS256", "ES256"))
+    def test_login_should_success(self, alg, public_key, private_key, secured_url, login_url):
+        code, content, headers = self.http_post(login_url, {self.USERNAME_FIELD:self.USERNAME, self.PASSWORD_FIELD:self.PASSWORD})
         
         # we expect return code 200, JSON content type
         self.assertEqual(code, 200)
@@ -20,7 +17,7 @@ class TestLogin(TestJWT):
         # we check if the JSON object is correct and token is valid
         received_object = json.loads(content)
         self.assertTrue("token" in received_object)
-        jwt_fields = self.decode_jwt_hs(received_object["token"], self.HMAC_SHARED_SECRET_LOGIN)
+        jwt_fields = self.decode_jwt(received_object["token"], public_key, alg)
         self.assertTrue(all(claim in jwt_fields for claim in [self.USERNAME_ATTRIBUTE, "exp", "nbf", "iat", "iss", "aud"]))
         self.assertEqual(jwt_fields[self.USERNAME_ATTRIBUTE], self.USERNAME)
         # we assume this test takes less than 1s
@@ -29,16 +26,18 @@ class TestLogin(TestJWT):
         self.assertEqual(jwt_fields["iss"], self.JWT_ISS)
         self.assertEqual(int(jwt_fields["nbf"]), int(jwt_fields["iat"])+self.JWT_NBF_DELAY)
 
-    def test_login_with_bad_credentials_should_fail(self):
-        code, content, headers = self.http_post(self.LOGIN_PATH, {self.USERNAME_FIELD:self.USERNAME, self.PASSWORD_FIELD:"azerty"})
+    @TestJWT.with_all_algorithms(algorithms=("HS256", "RS256", "ES256"))
+    def test_login_with_bad_credentials_should_fail(self, alg, public_key, private_key, secured_url, login_url):
+        code, content, headers = self.http_post(login_url, {self.USERNAME_FIELD:self.USERNAME, self.PASSWORD_FIELD:"azerty"})
         self.assertEqual(code, 401)
-        code, content, headers = self.http_post(self.LOGIN_PATH, {self.USERNAME_FIELD:self.USERNAME, self.PASSWORD_FIELD:""})
+        code, content, headers = self.http_post(login_url, {self.USERNAME_FIELD:self.USERNAME, self.PASSWORD_FIELD:""})
         self.assertEqual(code, 401)
-        code, content, headers = self.http_post(self.LOGIN_PATH, {self.USERNAME_FIELD:self.USERNAME})
+        code, content, headers = self.http_post(login_url, {self.USERNAME_FIELD:self.USERNAME})
         self.assertEqual(code, 401)
-        code, content, headers = self.http_post(self.LOGIN_PATH, {})
+        code, content, headers = self.http_post(login_url, {})
         self.assertEqual(code, 401)
 
-    def test_get_on_login_path_should_fail(self):
-        code, content, headers = self.http_get(self.LOGIN_PATH)
+    @TestJWT.with_all_algorithms(algorithms=("HS256", "RS256", "ES256"))
+    def test_get_on_login_path_should_fail(self, alg, public_key, private_key, secured_url, login_url):
+        code, content, headers = self.http_get(login_url)
         self.assertEqual(code, 405)
