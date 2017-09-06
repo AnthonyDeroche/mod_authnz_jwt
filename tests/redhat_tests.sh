7522cf50acd8aade636aaedcc6e5876065d5c2a2
brycehemme <bryce.hemme@gmail.com>
2017-09-06 03:29:14 +0000

    fix run tests permissions for non-root

--- tests/redhat_tests.sh
+++ tests/redhat_tests.sh
@@ -7,11 +7,11 @@ sudo touch /var/www/testjwt/index.html
 
 mkdir -p /opt/mod_jwt_tests
 
-openssl ecparam -name secp521r1 -genkey -noout -out /opt/mod_jwt_tests/ec-priv.pem
-openssl ec -in /opt/mod_jwt_tests/ec-priv.pem -pubout -out /opt/mod_jwt_tests/ec-pub.pem
+sudo openssl ecparam -name secp521r1 -genkey -noout -out /opt/mod_jwt_tests/ec-priv.pem
+sudo openssl ec -in /opt/mod_jwt_tests/ec-priv.pem -pubout -out /opt/mod_jwt_tests/ec-pub.pem
 
-openssl genpkey -algorithm RSA -out /opt/mod_jwt_tests/rsa-priv.pem -pkeyopt rsa_keygen_bits:4096
-openssl rsa -pubout -in /opt/mod_jwt_tests/rsa-priv.pem -out /opt/mod_jwt_tests/rsa-pub.pem
+sudo openssl genpkey -algorithm RSA -out /opt/mod_jwt_tests/rsa-priv.pem -pkeyopt rsa_keygen_bits:4096
+sudo openssl rsa -pubout -in /opt/mod_jwt_tests/rsa-priv.pem -out /opt/mod_jwt_tests/rsa-pub.pem
 
 sudo systemctl restart httpd
 
