789b22ff945294f29b5e55605819d7c90faaec53
Josef Barnes <joe@barnesmail.net>
2021-03-04 09:21:58 +1000

    Add config option to use a custom token name

--- tests/debian_tests.sh
+++ tests/debian_tests.sh
@@ -5,13 +5,16 @@ sudo cp jwt.htpasswd /var/www/jwt.htpasswd
 sudo mkdir -p /var/www/testjwt/
 sudo touch /var/www/testjwt/index.html
 
-mkdir -p /opt/mod_jwt_tests
+sudo mkdir -p /opt/mod_jwt_tests
 
 sudo openssl ecparam -name secp256k1 -genkey -noout -out /opt/mod_jwt_tests/ec-priv.pem
 sudo openssl ec -in /opt/mod_jwt_tests/ec-priv.pem -pubout -out /opt/mod_jwt_tests/ec-pub.pem
 
 sudo openssl genpkey -algorithm RSA -out /opt/mod_jwt_tests/rsa-priv.pem -pkeyopt rsa_keygen_bits:4096
 sudo openssl rsa -pubout -in /opt/mod_jwt_tests/rsa-priv.pem -out /opt/mod_jwt_tests/rsa-pub.pem
+
+sudo chmod 644 /opt/mod_jwt_tests/*.pem
+
 
 if ! sudo a2query -m rewrite > /dev/null; then
 	sudo a2enmod rewrite
